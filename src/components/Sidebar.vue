651a01fae048933c6ec5474d19389d3e32bcdf4b
AmayaGM <amayagalaviz@outlook.com>
2025-10-14 22:36:30 -0600

    sidebar styles

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -2,10 +2,26 @@
 <div class="sidebar">
     <SubirArchivo></SubirArchivo>
 <ul>
-    <li><RouterLink to="/Home">Inicio</RouterLink></li>
-    <li><RouterLink to="/MisDocumentos">Mis documentos</RouterLink></li>
-    <li><RouterLink to="/Destacados">Destacados</RouterLink></li>
-    <li><RouterLink to="/Papelera">Papelera</RouterLink></li>
+    <li class="menu-item">
+        <RouterLink to="/Home"> 
+            <i class="fa-solid fa-house"></i>   Inicio
+        </RouterLink>
+    </li>
+    <li class="menu-item">
+        <RouterLink to="/MisDocumentos">
+            <i class="fa-solid fa-folder"></i>   Mis documentos
+        </RouterLink>
+    </li>
+    <li class="menu-item">
+        <RouterLink to="/Destacados">
+            <i class="fa-solid fa-star"></i>   Destacados
+        </RouterLink>
+    </li>
+    <li class="menu-item">
+        <RouterLink to="/Papelera">
+            <i class="fa-solid fa-trash"></i>   Papelera
+        </RouterLink>
+    </li>
 </ul>
 </div>
 </template>
@@ -14,19 +30,55 @@
 import SubirArchivo from './modules/sidebar/SubirArchivo.vue';
 </script>
 
-<style>
+<style scoped>
+/* Estilos del contenedor principal */
 .sidebar{
     width: 100%;
     height: 100%;
     padding: 1rem;
 }
+
 .sidebar ul {
     list-style: none;
     padding: 0;
+    margin-top: 10%;
 }
-.sidebar a {
+
+/* Estilos base para el elemento de lista (LI) */
+.menu-item {
+    list-style: none;
+    background-color: var(--color-menu-background); /* Color normal */
+    color: var(--color-menu-text); /* Color normal */
+    transition: background-color 0.3s, color 0.3s;
+    cursor: pointer;
+}
+
+/* Estilos para el enlace (RouterLink) dentro del LI */
+.menu-item a {
     text-decoration: none;
-    display: block;
-    padding: 0.5rem 0;
+    display: block; /* Ocupa todo el LI para el área de clic */
+    padding: 10px 15px;
+    color: inherit; /* Hereda el color del texto del LI */
+    font-weight: normal; 
+    transition: background-color 0.3s, color 0.3s, font-weight 0.3s;
+}
+
+/* 🖱️ ESTADO HOVER (al pasar el mouse) */
+.menu-item:hover {
+    background-color: var(--color-menu-hover-background);
+    color: var(--color-menu-hove-text);
+    font-weight: normal; 
+}
+
+.menu-item a.router-link-active {
+    background-color: var(--color-menu-active-background);
+    color: var(--color-menu-active-text);
+    font-weight: bold; 
+}
+
+/* Sobrescribe el hover para mantener los colores activos cuando ya está seleccionado */
+.menu-item a.router-link-active:hover {
+    background-color: var(--color-menu-active-background);
+    color: var(--color-menu-active-text);
 }
 </style>
